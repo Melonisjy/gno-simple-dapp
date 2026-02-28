c7811e6bf61a9693185b98548cd7702ddf71ee36
Melonisjy <dhy2457@naver.com>
2026-03-01 01:50:03 +0900

    feat: add props on Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,22 @@
 type InputProps = {
   label: string;
   placeholder?: string;
+  value?: string;
+  onChange?: (value: string) => void;
 };
 
-export default function Input({ label, placeholder }: InputProps) {
+export default function Input({
+  label,
+  placeholder,
+  value,
+  onChange,
+}: InputProps) {
   return (
     <label className="block">
       <span className="mb-1 block">{label}</span>
       <input
+        value={value}
+        onChange={(e) => onChange?.(e.target.value)}
         placeholder={placeholder}
         className="h-10 w-full rounded-md border border-gray-400 px-3 text-[12px]"
       />
